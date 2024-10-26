29dd5b5c7aea6e5291f82b10b8f8726ded6fe79c
RioRio-do <riorio081121@gmail.com>
2024-10-26 17:13:15 +0900

    Refactor UnitCard to use Decimal for initialCount and fix autofocus bug

--- lib/unit_card.dart
+++ lib/unit_card.dart
@@ -1,5 +1,5 @@
+import 'package:calciunit/edit_unit_dialog.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -33,53 +33,7 @@ class UnitCard extends HookWidget {
         context: context,
         barrierDismissible: false,
         builder: (BuildContext context) {
-          return AlertDialog(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(0),
-            ),
-            title: const Text('数値を編集'),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                TextField(
-                  controller: controller,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(hintText: '新しい数値を入力'),
-                  focusNode: focusNode,
-                  autofocus: true,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text('キャンセル'),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        String? newValue = controller.text;
-                        count.value = newValue;
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text('保存'),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        Clipboard.setData(
-                            ClipboardData(text: count.value.toString()));
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('クリップボードにコピーされました')),
-                        );
-                      },
-                      child: const Text('コピー'),
-                    )
-                  ],
-                )
-              ],
-            ),
-          );
+          return editUnitDialog(controller, focusNode, context, count);
         },
       ).then((_) {
         focusNode.dispose();
